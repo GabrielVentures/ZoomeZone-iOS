47c11f1deab2664d4ee92b54997d5c0db82b424c
KentSun <kaifeng@base64.amsterdam>
2025-10-22 22:36:49 +0800

    Initial Commit

---
+++ ShelfTagSnap/ShelfTagSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShelfTagSnapApp.swift
+//  ShelfTagSnap
+//
+//  Created by kent.sun on 2025/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ShelfTagSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShelfTagSnapTests/ShelfTagSnapTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShelfTagSnapTests.swift
+//  ShelfTagSnapTests
+//
+//  Created by kent.sun on 2025/10/22.
+//
+
+import Testing
+@testable import ShelfTagSnap
+
+struct ShelfTagSnapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
